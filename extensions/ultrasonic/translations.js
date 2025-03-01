b95a6a5443e7dfeb31d0f0526436b44105e63d87
ArthurZheng <869705086@qq.com>
2025-03-01 22:12:04 +0800

    add support for ultrasonic module in realtime mode

--- extensions/ultrasonic/translations.js
+++ extensions/ultrasonic/translations.js
@@ -27,7 +27,24 @@ function getInterfaceTranslations () {
 }
 
 function registerScratchExtensionTranslations () {
-    return {};
+    return {
+        "en": {
+            "ultrasonic.categoryName": "Ultrasonic",
+            "ultrasonic.readDistance": "ultrasonic sensor pin TRIG [TRIG] ECHO [ECHO] read distance [UNIT]"
+        },
+        "ru": {
+            "ultrasonic.categoryName": "Ультразвуковой",
+            "ultrasonic.readDistance": "пины ультразвукового датчика TRIG [TRIG] ECHO [ECHO] расстояние считывания [UNIT]"
+        },
+        "zh-cn": {
+            "ultrasonic.categoryName": "超声波",
+            "ultrasonic.readDistance": "超声波传感器引脚 TRIG [TRIG] ECHO [ECHO] 读取距离 [UNIT]"
+        },
+        "zh-tw": {
+            "ultrasonic.categoryName": "超聲波",
+            "ultrasonic.readDistance": "超聲波傳感器管腳 TRIG [TRIG] ECHO [ECHO] 讀取距離 [UNIT]"
+        }
+    };
 }
 
 function registerBlocksMessages (Blockly) {
